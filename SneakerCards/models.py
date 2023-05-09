2ce112bfb8af689246a3beb0777d61d29e345711
nfomo <nfomo@iscte-iul.pt>
2023-05-09 16:22:17 +0100

    sqlLite DB migration done

--- SneakerCards/models.py
+++ SneakerCards/models.py
@@ -8,21 +8,23 @@ class Collector(models.Model):
     full_name = models.CharField(max_length=100)
     nickname = models.CharField(max_length=100)
     power = models.IntegerField()
+    credits = models.IntegerField(default=0)
     avatar = models.ImageField(upload_to='avatars', blank=True)
-    deck = models.ForeignKey('Deck', on_delete=models.CASCADE, null=True)
 
 
 class CardType(models.Model):
     descript = models.CharField(max_length=100)
     type = models.CharField(max_length=100)
     value = models.IntegerField()
+    saleValue = models.IntegerField(default=0)
+    percentage = models.FloatField(default=0.0)
 
 
 class Cards(models.Model):
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=100)
     card_type = models.ForeignKey(CardType, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to='images/cards/')
     description = models.CharField(max_length=100)
 
 
@@ -30,3 +32,4 @@ class Deck(models.Model):
     cards = models.ManyToManyField(Cards)
     name = models.CharField(max_length=100)
     power = models.IntegerField(null=True)
+    user = models.ForeignKey(Collector, on_delete=models.CASCADE)
